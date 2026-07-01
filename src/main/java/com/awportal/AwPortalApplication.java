efa52e36e0c5f43193ff50bb96d81d2b3591ad57
Juan Jiménez <juanjimenez@pricesmart.com>
2026-06-30 21:37:59 -0400

    Initialize Spring Boot project

---
+++ src/main/java/com/awportal/AwPortalApplication.java
@@ -0,0 +1,11 @@
+package com.awportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AwPortalApplication.class, args);
+    }
+}
